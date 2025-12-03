19ec99ed04d8e522eff499e8c041b9fd1e6431c6
sagun basnet <basnetsagun8@gmail.com>
2025-12-03 12:24:25 +0545

    useEffect

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import OnOf from "./components/OnOf";
 
 const App = () => {
-  const [count, setCount] = useState(false);
-  const handleClick = (color) => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
     setCount((prev) => prev + 1);
     // setCount((prev) => prev + 1);
     console.log(count);
   };
 
+  const welcomeFun = () => {
+    console.log("Welcome ");
+  };
+
+  useEffect(() => {
+    // if (count === 5) {
+    //   console.log("I am from useEffect");
+    // }
+    welcomeFun();
+  }, []);
+
   return (
-    // <div className="flex justify-center items-center w-full flex-col">
-    //   <button
-    //     onqClick={() => {
-    //       handleClick("red");
-    //       setCount(true);
-    //     }}
-    //     className="p-2 px-4 bg-blue-600 rounded-md"
-    //   >
-    //     Add
-    //   </button>
-    //   <br />
-    //   {count && <span>{count}</span>}
-    // </div>
-    <>
-      {/* <Login /> */}
-      {/* <Register /> */}
-      <OnOf />
-    </>
+    <div className="flex justify-center items-center w-full flex-col">
+      <button onClick={handleClick} className="p-2 px-4 bg-blue-600 rounded-md">
+        Add
+      </button>
+      <br />
+      <span>{count}</span>
+    </div>
   );
 };
 
